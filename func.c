e40e2e69a0dc5227f6a70face2b20fa1a0218ff9
Norbi Incze <incze.norbert@yahoo.com>
2017-01-06 21:40:36 +0200

    Add files via upload

--- func.c
+++ func.c
@@ -3,16 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct area {
-	int Ox, Oy, r;
-} area;
+#define PI 3.14159265358979323846
 
-struct pos {
+struct point {
     int x, y;
-} pos;
+} point;
 
+struct area {
+	int r;
+	struct point *o;
+} area;
+/**
+  * Returns the square of a number
+  * @param x is the input number
+  * @return the square of the {@code x}
+**/
 int sqr(int x) {
     return x * x;
+}
+/**
+  * Returns the distance between two points.
+  * @param a stores the position of the first point
+  * @param b stores the position of the second point
+  * @return the distance between A and B
+**/
+double distBetween(struct point *a, struct point *b) {
+    return sqrt(0.0 + sqr(a->x - b->x) + sqr(a->y - b->y));
 }
 
 void getColor(char *color, int *v) {
@@ -50,73 +66,203 @@ void getColor(char *color, int *v) {
 	}
 }
 /**
-  * Tells us if we win the game.
+  * Tells us if we won the game.
   * @param e stores the position of the enemy
   * @param c stores the attributes of the circle
   * @return 1 if the enemy is outside of the circle
 **/
-int checkWinCondition(struct pos e, struct area c) {
-	if(c.r * c.r < (e.x - c.Ox) * (e.x - c.Ox) + (e.y - c.Oy) * (e.y - c.Oy))
-        return 1;
-    else
-        return 0;
-}
-
-int checkSafeZone(struct pos o, struct area c) {
-    if(sqr((c.r - 15)) > sqr((o.x - c.Ox)) + sqr((o.y - c.Oy)))
+int checkWinCondition(struct point *e, struct area *c) {
+	if(distBetween(e, c->o) > c->r)
         return 1;
     else
         return 0;
 }
 /**
-  * Tells us the distance between the D point and the OP straight.
-  * @param o stores our position
-  * @param p stores the enemy's position
+  * Tells us we are in safe zone or not.
+  * @param o stores our robot's position
+  * @param d stores our direction's position
+  * @param c stores the attributes of the circle
+  * @return 1 if we are in safe zone
+**/
+int checkSafeZone(struct point *o, struct point *d, struct area *c) {
+    if(distBetween(o, c->o) < c->r - 2 * distBetween(o, d))
+        return 1;
+    else
+        return 0;
+}
+/**
+  * Tells us the distance between a point and a straight.
   * @param d stores a point's position
-  * @return the distance between the D point and the OP straight
+  * @param a stores the first point's position
+  * @param b stores the second point's position
+  * @return the distance between the D point and the AB straight
 **/
-int onStraight(struct pos o, struct pos p, struct pos d) {
-    int a = p.x - o.x, b = -(p.y - o.y), c = o.x * a + o.y * b;
-    return abs(a * d.x + b * d.y + c) / sqrt(a * a + b * b);
+double onStraight(struct point *d, struct point *a, struct point *b) {
+    int nx, ny, c;
+    nx = b->x - a->x;
+    ny = -(b->y - a->y);
+    c = a->x * nx + a->y * ny;
+    return abs(0.0 + nx * d->x + ny * d->y + c) / sqrt(0.0 + sqr(nx) + sqr(ny));
 }
 /**
-  * Tells us the distance between two points.
-  * @param a stores the position of the first point
-  * @param b stores the position of the second point
-  * @return the distance between A and B
+  * Returns the angle of 3 points
+  * @param a is the first point
+  * @param b is the second point which angle will be returned
+  * @param c is the third point
+  * @return angle of ABC
 **/
-int distBetween(struct pos a, struct pos b) {
-    return sqrt(sqr(a.x - b.x) + sqr(a.y - b.y));
-}
+double getAngle(struct point *a, struct point *b, struct point *c) {
+    double atfogo, aux;
 
-int getAngle(int befogo, int atfogo) {
-    return befogo / atfogo;
-}
-
-char *chooseAction(struct area c, struct pos o, struct pos d, struct pos e) {
-    int dist, eps;
-
-    if((dist = distBetween(o, e)) > 3 * c.r / 4) //
-        eps = 15;
-    else if(dist > c.r / 2)
-        eps = 10;
+    if(distBetween(a, b) > distBetween(b, c))
+        atfogo = distBetween(a, b);
     else
-        eps = 5;
+        atfogo = distBetween(b, c);
 
-    if(onStraight(o, e, d) - eps <= 0) {
+    aux = onStraight(b, a, c) / atfogo;
+    if(aux <= -1 || aux >= 1)
+        return 0;
 
+    return asin(onStraight(b, a, c) / atfogo) * 180 / PI;
+}
+/**
+  * Tells us what our robot need to do.
+  * @param c stores the attributes of the circle
+  * @param o stores our position
+  * @param d stores our direction's position
+  * @param e stores the enemy's position
+  * @return the letter of the selected action
+**/
+char *chooseAction(struct area *c, struct point *o, struct point *d, struct point *e) {
+    int i, index = 0, dr = distBetween(o, d);
+    struct point *dless, *dmore;
+    double radian = 2 * PI, DOE, EDO, DEO;
+
+    EDO = getAngle(e, d, o);
+    DEO = getAngle(d, e, o);
+    if(!EDO) {
+        DOE = 0;
+    }
+    else if(!DEO) {
+        DOE = 90;
+    }
+    else
+        DOE = 180 - getAngle(e, d, o) - getAngle(d, e, o);
+
+    for(i = 0; i <= round(radian * dr); i++)
+        if(d->x <= 0.5 + c->o->x + o->x + sin(radian * (i / (2 * dr * PI))) * dr  &&  d->y <= 0.5 + c->o->y + o->y + cos(radian * (i / (2 * dr * PI))) * dr) {
+            index = i;
+            break;
+        }
+
+    printf("DOE %f %f %f\n", DOE, getAngle(e, d, o), getAngle(d, e, o));
+
+
+
+    dmore = malloc(sizeof(struct point));
+    dless = malloc(sizeof(struct point));
+    if(!dless) {
+        printf("Allocaton error!\n");
+        exit(-1);
     }
 
+    dless->x = c->o->x + o->x + sin(2 * PI * ((index - i / 8) / (2 * dr * PI))) * dr;
+    dless->y = c->o->y + o->y + cos(2 * PI * ((index - i / 8) / (2 * dr * PI))) * dr;
+
+    dmore->x = c->o->x + o->x + sin(2 * PI * ((index + i / 8) / (2 * dr * PI))) * dr;
+    dmore->y = c->o->y + o->y + cos(2 * PI * ((index + i / 8) / (2 * dr * PI))) * dr;
+
+    if(DOE <= 5) {
+        if(distBetween(d, e) < distBetween(o, e))
+            return "f";
+        else if(distBetween(o, e) < c->r / 2)
+            return "b";
+    }
+
+    if(distBetween(dless, e) < distBetween(dmore, e)) {
+        return "r";
+    }
+    else
+        return "l";
 }
+/**
+  * This function makes precisely our robot's action
+  * @param c stores the attributes of the circle
+  * @param o stores our position
+  * @param d stores our direction's position
+  * @param e stores the enemy's position
+  * @return what our robot's need to do
+**/
+char *moveRobot(struct area *c, struct point *o, struct point *d, struct point *e) {
+    char *moves = malloc(100 * sizeof(char));
+    int i;
+    double DOE = 180 - getAngle(o, e, d) - getAngle(o, d, e);
 
-char *moveRobot(struct area c, struct pos o, struct pos d, struct pos e) {
-    int dr = sqrt(sqr(o.x - c.Ox + d.x - c.Ox) + sqr(o.y - c.Oy + d.y - c.Oy));
-    int dir = onStraight(o, e, d);
+    if(!moves) {
+        printf("Allocation error!\n");
+        exit(-1);
+    }
+    strcpy(moves, chooseAction(c, o, d, e));
 
+    if(moves[0] == 'f') {
+        if(distBetween(o, e) <= c->r / 2 && distBetween(o, c->o) <= c->r / 2)
+            strcat(moves, "fffff");
+        else
+            strcat(moves, "ff");
+    }
+    else if(moves[0] == 'b') {
+        if(distBetween(o, c->o) <= c->r / 2)
+            strcat(moves, "bbbbb");
+        else
+            strcat(moves, "bb");
+    }
+    else {
+        for(i = 20; i < DOE; i += 20)
+            if(moves[0] == 'l')
+                strcat(moves, "l");
+            else
+                strcat(moves, "r");
+    }
 
-    return "s";
+    strcat(moves, "s");
+    return moves;
 }
 
 int main(void) {
+    struct point *o, *e, *d;
+    struct area *c;
+
+    o = malloc(sizeof(struct point));
+    e = malloc(sizeof(struct point));
+    d = malloc(sizeof(struct point));
+    c = malloc(sizeof(struct area));
+
+    if(!o || !e || !d || !c) {
+        printf("Allocation error!\n");
+        return 1;
+    }
+
+    c->o = malloc(sizeof(struct point));
+    if(!c->o) {
+        printf("Allocation error!\n");
+        return 1;
+    }
+
+    c->o->x = 0;
+    c->o->y = 0;
+    c->r = 100;
+
+    e->x = 0;
+    e->y = 0;
+
+    o->x = 50;
+    o->y = 0;
+
+    d->x = 50;
+    d->y = -20;
+
+    printf("%s\n", moveRobot(c, o, d, e));
+
+
     return 0;
 }
